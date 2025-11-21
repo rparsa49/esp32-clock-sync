baccc7159acaa47ebad63daf0681368ce3de6db3
roya parsa <99053997+rparsa49@users.noreply.github.com>
2025-11-21 18:02:51 -0500

    Fix comment for clarity on script improvements

--- client.ino
+++ client.ino
@@ -1,5 +1,5 @@
 /* Some parts of our script were drafted and refined with assistance from GPT-5 Thinking.
-    These included performing the handshake, .... 
+    These included performing the handshake and HMAC creation 
     We then made our own improvements and justifications based on testing and observation. 
 */
 
